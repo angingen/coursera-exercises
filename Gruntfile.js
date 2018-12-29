5301f76ed829753d527f101a460558f8cfbd5d73
Jingwen Wang <w.jingwen@yahoo.com>
2018-12-29 18:38:54 +1100

    grunt part 1

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+'use strict'; // use strict javascript code
+
+module.exports = function(grunt) {
+	// set up Grunt configuration for all the tasks
+
+	require('time-grunt')(grunt);
+
+	require('jit-grunt')(grunt);
+
+	grunt.initConfig({
+		// add in the configuration for the wheel's grunt tasks
+		// it is a javascript object
+
+		sass: {
+			dist: {
+				files: {
+					'css/styles.css': 'css/styles.scss'
+				}
+			}
+		}, 
+
+		watch: {
+			files: 'css/*.scss',
+			tasks: ['sass']
+		},
+		browserSync: { 
+			dev: {
+				bsFiles: {
+					scr: [
+						'css/*.css',
+						'*.html',
+						'js/*.js'
+					]
+				},
+				options: {
+					watchTask: true, 
+					server: {
+						baseDir: "./" 
+					}
+				}
+			}	
+		}
+
+	});
+
+
+	grunt.registerTask('css',['sass']);
+	grunt.registerTask('default', ['browserSync', 'watch']); 
+};
